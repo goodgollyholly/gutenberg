80fac6db1f525b5fb4299745392afd5b68d0a5b1
Ramon <ramonjd@gmail.com>
2025-09-22 15:04:25 +1000

    Refactor block editor components to remove zoom out mode dependencies
    - Simplified state management in block-list and related components by removing isZoomOut checks.
    - Updated logic in block settings dropdown and block tools to enhance performance and maintainability.
    - Cleaned up unused imports and adjusted related functionality to ensure consistent behavior without zoom out mode.

--- packages/block-editor/src/store/reducer.js
+++ packages/block-editor/src/store/reducer.js
@@ -2282,8 +2282,6 @@ function getDerivedBlockEditingModesForTree(
 	isNavMode = false,
 	treeClientId = ''
 ) {
-	const isZoomedOut =
-		state?.zoomLevel < 100 || state?.zoomLevel === 'auto-scaled';
 	const derivedBlockEditingModes = new Map();
 
 	// When there are sections, the majority of blocks are disabled,
@@ -2367,7 +2365,7 @@ function getDerivedBlockEditingModesForTree(
 			}
 		}
 
-		if ( isZoomedOut || isNavMode ) {
+		if ( isNavMode ) {
 			// If the root block is the section root set its editing mode to contentOnly.
 			if ( clientId === sectionRootClientId ) {
 				derivedBlockEditingModes.set( clientId, 'contentOnly' );
@@ -2382,13 +2380,6 @@ function getDerivedBlockEditingModesForTree(
 
 			if ( sectionClientIds.includes( clientId ) ) {
 				derivedBlockEditingModes.set( clientId, 'contentOnly' );
-				return;
-			}
-
-			// If zoomed out, all blocks that aren't sections or the section root are
-			// disabled.
-			if ( isZoomedOut ) {
-				derivedBlockEditingModes.set( clientId, 'disabled' );
 				return;
 			}
 
@@ -3029,9 +3020,9 @@ export function withDerivedBlockEditingModes( reducer ) {
 				break;
 			}
 			case 'RESET_BLOCKS':
-			case 'SET_EDITOR_MODE':
-			case 'RESET_ZOOM_LEVEL':
-			case 'SET_ZOOM_LEVEL': {
+			case 'SET_EDITOR_MODE': {
+				// case 'RESET_ZOOM_LEVEL':
+				// case 'SET_ZOOM_LEVEL': {
 				// Recompute the entire tree if the editor mode or zoom level changes,
 				// or if all the blocks are reset.
 				return {
--- packages/edit-post/src/components/layout/index.js
+++ packages/edit-post/src/components/layout/index.js
@@ -463,7 +463,7 @@ function Layout( {
 				kind: 'postType',
 				name: 'wp_template',
 			} );
-			const { getBlockSelectionStart, isZoomOut } = unlock(
+			const { getBlockSelectionStart } = unlock(
 				select( blockEditorStore )
 			);
 			const {
@@ -492,9 +492,8 @@ function Layout( {
 				hasBlockSelected: !! getBlockSelectionStart(),
 				showIconLabels: get( 'core', 'showIconLabels' ),
 				isDistractionFree: get( 'core', 'distractionFree' ),
-				showMetaBoxes:
-					( isNotDesignPostType && ! isZoomOut() ) ||
-					isDirectlyEditingPattern,
+				// @ZOOMOUTMODE
+				showMetaBoxes: isNotDesignPostType || isDirectlyEditingPattern,
 				isWelcomeGuideVisible: isFeatureActive( 'welcomeGuide' ),
 				templateId:
 					supportsTemplateMode &&
@@ -503,8 +502,9 @@ function Layout( {
 					! isEditingTemplate
 						? _templateId
 						: null,
+				// @ZOOMOUTMODE
 				enablePaddingAppender:
-					! isZoomOut() && isRenderingPostOnly && isNotDesignPostType,
+					isRenderingPostOnly && isNotDesignPostType,
 				isDevicePreview: getDeviceType() !== 'Desktop',
 			};
 		},
