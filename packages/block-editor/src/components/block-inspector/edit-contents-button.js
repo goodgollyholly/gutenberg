0e8507ccb685f6b3f9af444309ce063b99f483a7
Daniel Richards <daniel.richards@automattic.com>
2025-10-03 13:49:05 +0800

    Fix focus mode restoration

--- packages/block-editor/src/components/block-inspector/edit-contents-button.js
+++ packages/block-editor/src/components/block-inspector/edit-contents-button.js
@@ -15,7 +15,7 @@ export default function EditContentsButton( { clientId } ) {
 	// Disable reason: it is an effect so can't move relocated to after
 	// the if statement.
 	// eslint-disable-next-line @wordpress/no-unused-vars-before-return
-	const { modifyContentLockBlock } = unlock(
+	const { modifyContentLockBlock, stopEditingAsBlocks } = unlock(
 		useDispatch( blockEditorStore )
 	);
 	const { attributes, editedClientId } = useSelect(
@@ -47,7 +47,7 @@ export default function EditContentsButton( { clientId } ) {
 				if ( ! editedClientId ) {
 					modifyContentLockBlock( clientId );
 				} else {
-					modifyContentLockBlock();
+					stopEditingAsBlocks();
 				}
 			} }
 		>
