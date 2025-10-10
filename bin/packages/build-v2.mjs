c911f6052f892786e3a3519fe5a5b6f58892a350
Riad Benguella <benguella@gmail.com>
2025-10-10 22:18:12 +0100

    Build V2: Migrate more packages including block-editor

--- bin/packages/build-v2.mjs
+++ bin/packages/build-v2.mjs
@@ -404,6 +404,10 @@ function wordpressExternalsPlugin(
 					outputDir,
 					`${ assetName }.asset.php`
 				);
+
+				await mkdir( path.dirname( assetFilePath ), {
+					recursive: true,
+				} );
 				await writeFile( assetFilePath, assetContent );
 			} );
 		},
